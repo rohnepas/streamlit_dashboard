5161241b3d440c3d2098eb16b539121488311179
Pascal Rohner <pascal@prohner.com>
2024-08-07 22:45:34 +0200

    round to whole numbers

--- scheduler.py
+++ scheduler.py
@@ -17,7 +17,7 @@ def send_trading_signal(df_merged):
 
     if sendTelegramMessage:
         signal_value = df_merged["signal"].iloc[-1].upper()  # Convert signal value to uppercase
-        price = df_merged["close"].iloc[-1]
+        price = round(df_merged["close"].iloc[-1])  # Round price to the nearest whole number
         fear_and_greed_value = df_merged["value"].iloc[-1]
         
         # Use pytz to get the current time in Zurich
@@ -27,12 +27,12 @@ def send_trading_signal(df_merged):
         formatted_message = (
             f"BTC Trading Dashboard on {now}:\n"
             f"Signal: <b>{signal_value}</b>\n"
-            f"Price: <b>{price}</b>\n"
+            f"Price: <b>{price}</b> CHF\n"
             f"Fear and Greed Index: <b>{fear_and_greed_value}</b>"
         )
         
         send_telegram_message(os.getenv("BOT_ID"), os.getenv("CHAT_ID"), formatted_message)
-
+        
 if __name__ == "__main__":
     success, message, df_merged = fetch_and_process_data()
     if success:
