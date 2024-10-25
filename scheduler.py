face41b406afd9387c0ef2a5fedc2c8448a48083
Pascal Rohner <pascal@prohner.com>
2024-10-25 21:27:08 +0200

    Send message when fetching failed.

--- scheduler.py
+++ scheduler.py
@@ -9,34 +9,43 @@ from config import TELEGRAM
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def send_trading_signal(df_merged):
+def send_trading_signal(df_merged=None, error_message=None):
     try:
-        logging.info("Preparing to send trading signal...")
-        sendTelegramMessage = TELEGRAM.get("SEND_MESSAGE", False)
+        if error_message:
+            logging.info("Preparing to send error notification signal...")
+            formatted_message = (
+                f"BTC Trading Dashboard Alert:\n"
+                f"Error: <b>{error_message}</b>"
+            )
+        else:
+            logging.info("Preparing to send trading signal...")
+            sendTelegramMessage = TELEGRAM.get("SEND_MESSAGE", False)
 
-        if sendTelegramMessage:
+            if not sendTelegramMessage:
+                logging.warning("Telegram message sending is disabled in the configuration.")
+                return
+
             # Retrieve and format signal information
             signal_value = df_merged["signal"].iloc[-1].upper()  # Convert signal value to uppercase
             price = round(df_merged["close"].iloc[-1])  # Round price to the nearest whole number
             fear_and_greed_value = df_merged["value"].iloc[-1]
-            
+
             # Use pytz to get the current time in Zurich
             tz = pytz.timezone('Europe/Zurich')
             now = datetime.now(tz).strftime("%d.%m.%Y %H:%M:%S")
-            
+
             formatted_message = (
                 f"BTC Trading Dashboard on {now}:\n"
                 f"Signal: <b>{signal_value}</b>\n"
                 f"Price: <b>{price}</b> USD\n"
                 f"Fear and Greed Index: <b>{fear_and_greed_value}</b>"
             )
-            
+
             logging.info("Trading signal message prepared: %s", formatted_message)
-            
-            send_telegram_message(os.getenv("BOT_ID"), os.getenv("CHAT_ID"), formatted_message)
-            logging.info("Trading signal sent successfully.")
-        else:
-            logging.warning("Telegram message sending is disabled in the configuration.")
+
+        # Send the message
+        send_telegram_message(os.getenv("BOT_ID"), os.getenv("CHAT_ID"), formatted_message)
+        logging.info("Trading signal sent successfully.")
     except Exception as e:
         logging.exception("An error occurred while sending the trading signal: %s", e)
 
@@ -49,5 +58,6 @@ if __name__ == "__main__":
             send_trading_signal(df_merged)
         else:
             logging.error("Failed to fetch and process data: %s", message)
+            send_trading_signal(error_message=f"Failed to fetch and process data: {message}")
     except Exception as e:
         logging.exception("An unexpected error occurred in the main function: %s", e)
